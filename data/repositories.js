0a6d0735056dc77e11e3ba6180239ce4d056dfbf
localredhead <levi.strope@gmail.com>
2019-02-06 18:08:07 -0500

    Add uport-mobile to releases

--- data/repositories.js
+++ data/repositories.js
@@ -1,4 +1,5 @@
 module.exports = {
   'uport-connect': 'MDEwOlJlcG9zaXRvcnk4MDU1NzkxOQ==',
-  'uport-credentials': 'MDEwOlJlcG9zaXRvcnk3ODEzNzA3Nw=='
+  'uport-credentials': 'MDEwOlJlcG9zaXRvcnk3ODEzNzA3Nw==',
+  'uport-mobile': 'MDEwOlJlcG9zaXRvcnkxNTUzNTcxMjE='
 }
