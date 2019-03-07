276a8f7c498a8730966a37c494c1416b8faaecb6
Subir <schowdhuri@users.noreply.github.com>
2019-03-08 02:08:06 +0530

    [#162751564] Custom 404 Page Redesign (#222)
    * Custom 404 page redesigned
    
    * Cleanup

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,18 @@
-import React from "react"
+import React from 'react'
 import { Link } from 'gatsby'
-import Helmet from "react-helmet"
-import styled from "styled-components"
+import Helmet from 'react-helmet'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import SiteHeader from '../components/Layout/Header'
-import SiteFooter from '../components/Layout/Footer'
-import config from "../../data/SiteConfig"
-import MainHeader from '../components/Layout/Header'
-import About from '../components/About/About'
+import config from '../../data/SiteConfig'
 import { medium } from '../layouts/grid'
-import arrowImg from '../images/ArrowBlurple.png'
+import bgImg from '../images/404-bg.svg'
 
-class AboutPage extends React.Component {
+class Custom404 extends React.Component {
   render() {
     return (<Layout location={this.props.location}>
-      <div className="index-container">
+      <div className='index-container'>
         <Helmet title={config.siteTitle} />
         <Main>
           <IndexHeadContainer>
@@ -25,15 +22,28 @@ class AboutPage extends React.Component {
           </IndexHeadContainer>
           <BodyContainer>
             <Message>
-              <h1>We're sorry. The page you're looking for can't be found.</h1>
-              <Link to='/' className='home'>
-                Take me Home
-              </Link>
+              <h2>So, this happened...</h2>
+              <h1>404</h1>
+              <h3>We can’t seem to find the page you’re looking for.</h3>
             </Message>
           </BodyContainer>
-          {/*<FooterContainer>
-            <SiteFooter />
-          </FooterContainer>*/}
+          <Footer>
+            <p>Here are some links that might be helpful:</p>
+            <ul>
+              <li>
+                <Link to='/'>Home &#8594;</Link>
+              </li>
+              <li>
+                <Link to='/categories/uport-connect'>uport-connect &#8594;</Link>
+              </li>
+              <li>
+                <Link to='/categories/uport-credentials'>uport-credentials &#8594;</Link>
+              </li>
+              <li>
+                <Link to='/categories/uport-transports'>uport-transports &#8594;</Link>
+              </li>
+            </ul>
+          </Footer>
         </Main>
       </div>
     </Layout>)
@@ -43,39 +53,61 @@ class AboutPage extends React.Component {
 const Main = styled.main`
   display: flex;
   flex-direction: column;
-  // justify-content: space-between;
+  justify-content: space-between;
   min-height: 100vh;
 `
 const IndexHeadContainer = styled.div`
   background: ${props => props.theme.brand};
 `
 const BodyContainer = styled.div`
+  background: transparent url(${bgImg}) center no-repeat;
+  background-size: cover;
   padding: ${props => props.theme.sitePadding};
 `
 const Message = styled.section`
-  margin: 10vh auto 0;
-  padding: 20px;
-  ${medium('width: 60vw;')}
+  text-align: center;
+  padding: 2rem 20px;
+  ${medium('padding: 100px 0;')}
 
   h1 {
-    font-size: 1.5rem;
+    color: #5C50CA;
+    font-size: 5rem;
+    line-height: 1;
+    margin: 40px 0;
+    ${medium('font-size: 11.25rem;')}
   }
-  .home {
-    border: 2px solid #5c50ca;
-    border-radius: 4px;
-    background: linear-gradient(180deg,#7958D8 0%,#5C50CA 100%);
-    color: #fff;
-    box-shadow: 0 2px 10px 0 rgba(63,61,75,0.2);
-    display: inline-block;
-    padding: 10px 15px;
-    font-weight: 600;
-    text-align: center;
-    text-decoration: none;
+  h2 {
+    color: #5C50CA;
+    font-size: 2.375rem;
+    line-height: 1;
+  }
+  h3 {
+    color: #5C50CA;
+    font-size: 1.5rem;
+    line-height: 1;
   }
 `;
-const FooterContainer = styled.footer`
-  background-color: #6c59cf;
+const Footer = styled.footer`
+  background: linear-gradient(138.95deg, #5C50CA 5.42%, #7958D8 96.28%);
   clear: all;
+  color: #fff;
+  padding: 35px 20px;
+  text-align: center;
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+  li + li {
+    margin-top: 15px;
+  }
+  a {
+    color: #fff;
+    font-size: 1rem;
+    font-weight: 600;
+    text-decoration: none;
+  }
 `
 
-export default AboutPage;
+export default Custom404
